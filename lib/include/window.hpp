8342349b7614d963ece19eed8f7d52363a486ccd
Humeur <laetitiapaypal@gmail.com>
2021-05-17 00:02:08 +0200

    All the arguments are now const reference in Window encapsulation

--- lib/include/window.hpp
+++ lib/include/window.hpp
@@ -21,9 +21,9 @@ namespace Luxray {
             const std::string title;
         public:
             // Initialize window and OpenGL context
-            Window(int width = 1920,
-                   int height = 1080,
-                   const std::string title = "Default Window")
+            Window(const int &width = 1920,
+                   const int & height = 1080,
+                   const std::string &title = "Default Window")
                    : width(width), height(height), title(title) {
                 ::InitWindow(width, height, title.c_str());
             }
@@ -78,18 +78,18 @@ namespace Luxray {
             }
 
             // Check if one specific window flag is enabled
-            inline bool haveState(unsigned int flag) {
+            inline bool haveState(const unsigned int &flag) {
                 return ::IsWindowState(flag);
             }
 
             // Set window configuration state using flags
-            inline Window &setState(unsigned int flag) {
+            inline Window &setState(const unsigned int &flag) {
                 ::SetWindowState(flag);
                 return *this;
             }
 
             // Clear window configuration state flags
-            inline Window &clearState(unsigned int flags) {
+            inline Window &clearState(const unsigned int &flags) {
                 ::ClearWindowState(flags);
                 return *this;
             }
@@ -119,38 +119,40 @@ namespace Luxray {
             }
 
             // Set icon for window (only PLATFORM_DESKTOP)
-            inline Window &setIcon(Image image) {
+            inline Window &setIcon(const Image &image) {
                 //TODO replace Image by a Luxray object image
                 ::SetWindowIcon(image);
                 return *this;
             }
 
             // Set title for window (only PLATFORM_DESKTOP)
-            inline Window &setTitle(const std::string title) {
+            inline Window &setTitle(const std::string &title) {
                 ::SetWindowTitle(title.c_str());
                 return *this;
             }
 
             // Set window position on screen (only PLATFORM_DESKTOP)
-            inline Window &setPosition(int x, int y) {
+            inline Window &setPosition(const int &x, const int &y) {
                 ::SetWindowPosition(x, y);
                 return *this;
             }
 
             // Set monitor for the current window (fullscreen mode)
-            inline Window &setMonitor(int monitor) {
+            inline Window &setMonitor(const int &monitor) {
                 ::SetWindowMonitor(monitor);
                 return *this;
             }
 
             // Set window minimum dimensions (for FLAG_WINDOW_RESIZABLE)
-            inline Window &setMinSize(int width, int height) {
+            inline Window &setMinSize(const int &width, const int &height) {
                 ::SetWindowMinSize(width, height);
                 return *this;
             }
 
             // Set window dimensions
-            inline Window &setSize(int width, int height) {
+            inline Window &setSize(const int &width, const int &height) {
+                this->width = width;
+                this->height = height;
                 ::SetWindowSize(width, height);
                 return *this;
             }
@@ -181,33 +183,33 @@ namespace Luxray {
             }
 
             // Get specified monitor position
-            inline Vector2 getMonitorPosition(int monitor) {
+            inline Vector2 getMonitorPosition(const int &monitor) {
                 //TODO replace Vector2 by Luxray object Vector2
                 return ::GetMonitorPosition(monitor);
             }
 
             // Get specified monitor width (max available by monitor)
-            inline int getMonitorWidth(int monitor) {
+            inline int getMonitorWidth(const int &monitor) {
                 return ::GetMonitorWidth(monitor);
             }
 
             // Get specified monitor height (max available by monitor)
-            inline int getMonitorHeight(int monitor) {
+            inline int getMonitorHeight(const int &monitor) {
                 return ::GetMonitorHeight(monitor);
             }
 
             // Get specified monitor physical width in millimetres
-            inline int getMonitorPhysicalWidth(int monitor) {
+            inline int getMonitorPhysicalWidth(const int &monitor) {
                 return ::GetMonitorPhysicalWidth(monitor);
             }
 
             // Get specified monitor physical height in millimetres
-            inline int getMonitorPhysicalHeight(int monitor) {
+            inline int getMonitorPhysicalHeight(const int &monitor) {
                 return ::GetMonitorPhysicalHeight(monitor);
             }
 
             // Get specified monitor refresh rate
-            inline int getMonitorRefreshRate(int monitor) {
+            inline int getMonitorRefreshRate(const int &monitor) {
                 return ::GetMonitorRefreshRate(monitor);
             }
 
@@ -223,13 +225,13 @@ namespace Luxray {
             }
 
             // Get the human-readable, UTF-8 encoded name of the primary monitor
-            inline const std::string getMonitorName(int monitor) {
+            inline const std::string getMonitorName(const int &monitor) {
                 const std::string monitorName(::GetMonitorName(monitor));
                 return monitorName;
             }
 
             // Set clipboard text content
-            inline Window &setClipboard(const std::string text) {
+            inline Window &setClipboard(const std::string &text) {
                 ::SetClipboardText(text.c_str());
                 return *this;
             }
@@ -241,7 +243,7 @@ namespace Luxray {
             }
 
             // Set target FPS (maximum)
-            inline Window &setTargetFPS(int fps) {
+            inline Window &setTargetFPS(const int &fps) {
                 ::SetTargetFPS(fps);
                 return *this;
             }
@@ -262,7 +264,7 @@ namespace Luxray {
             }
 
             // Set background color (framebuffer clear color)
-            static inline void clearBackground(Color color = WHITE) {
+            static inline void clearBackground(const Color &color = WHITE) {
                 ::ClearBackground(color);
             }
 
@@ -277,7 +279,7 @@ namespace Luxray {
             }
 
             // Initialize 2D mode with custom camera (2D)
-            static inline void beginMode2D(Camera2D camera) {
+            static inline void beginMode2D(const Camera2D &camera) {
                 ::BeginMode2D(camera);
             }
 
@@ -287,7 +289,7 @@ namespace Luxray {
             }
 
             // Initializes 3D mode with custom camera (3D)
-            static inline void beginMode3D(Camera3D camera) {
+            static inline void beginMode3D(const Camera3D &camera) {
                 ::BeginMode3D(camera);
             }
 
@@ -297,7 +299,7 @@ namespace Luxray {
             }
 
             // Initializes render texture for drawing
-            static inline void beginTextureMode(RenderTexture2D target) {
+            static inline void beginTextureMode(const RenderTexture2D &target) {
                 ::BeginTextureMode(target);
             }
 
@@ -307,7 +309,7 @@ namespace Luxray {
             }
 
             // Begin custom shader drawing
-            static inline void beginShaderMode(Shader shader) {
+            static inline void beginShaderMode(const Shader &shader) {
                 ::BeginShaderMode(shader);
             }
 
@@ -317,7 +319,7 @@ namespace Luxray {
             }
 
             // Begin blending mode (alpha, additive, multiplied)
-            static inline void beginBlendMode(int mode) {
+            static inline void beginBlendMode(const int &mode) {
                 ::BeginBlendMode(mode);
             }
 
@@ -327,7 +329,7 @@ namespace Luxray {
             }
 
             // Begin scissor mode (define screen area for following drawing)
-            static inline void beginScissorMode(int x, int y, int width, int height) {
+            static inline void beginScissorMode(const int &x, const int &y, const int &width, const int &height) {
                 ::BeginScissorMode(x, y, width, height);
             }
 
@@ -386,56 +388,56 @@ namespace Luxray {
             }
 
             //trigger beginDrawing then fun then end drawing, a all in one drawing function
-            static inline void draw(std::function<void()> fun) {
+            static inline void draw(const std::function<void()> &fun) {
                 Window::beginDrawing();
                 fun();
                 Window::endDrawing();
             }
 
             //trigger beginMode2D then fun then endMode2D, a all in one mode2D function
-            static inline void mode2D(Camera2D camera, std::function<void()> fun) {
+            static inline void mode2D(const Camera2D &camera, const std::function<void()> &fun) {
                 Window::beginMode2D(camera);
                 fun();
                 Window::endMode2D();
             }
 
             //trigger beginMode3D then fun then endMode3D, a all in one mode3D function
-            static inline void mode3D(Camera3D camera, std::function<void()> fun) {
+            static inline void mode3D(const Camera3D &camera, const std::function<void()> &fun) {
                 Window::beginMode3D(camera);
                 fun();
                 Window::endMode3D();
             }
 
             //trigger beginTextureMode then fun then endTextureMode, a all in one textureMode function
-            static inline void textureMode(RenderTexture2D target, std::function<void()> fun) {
+            static inline void textureMode(const RenderTexture2D &target, const std::function<void()> &fun) {
                 Window::beginTextureMode(target);
                 fun();
                 Window::endTextureMode();
             }
 
             //trigger beginShaderMode then fun then endShaderMode, a all in one shaderMode function
-            static inline void shaderMode(Shader shader, std::function<void()> fun) {
+            static inline void shaderMode(const Shader &shader, const std::function<void()> &fun) {
                 Window::beginShaderMode(shader);
                 fun();
                 Window::endShaderMode();
             }
 
             //trigger beginBlendMode then fun then endBlendMode, a all in blendMode function
-            static inline void blendMode(int mode, std::function<void()> fun) {
+            static inline void blendMode(const int &mode, const std::function<void()> &fun) {
                 Window::beginBlendMode(mode);
                 fun();
                 Window::endBlendMode();
             }
 
             //trigger beginScissorMode then fun then endScissorMode, a all in one scissorMode function
-            static inline void scissorMode(int x, int y, int width, int height, std::function<void()> fun) {
+            static inline void scissorMode(const int &x, const int &y, const int &width, const int &height, const std::function<void()> &fun) {
                 Window::beginScissorMode(x, y, width, height);
                 fun();
                 Window::endScissorMode();
             }
 
             //trigger beginVrStereoMode then fun then endVrStereoMode, a all in one drawing function
-            static inline void vrStereoMode(VrStereoConfig config, std::function<void()> fun) {
+            static inline void vrStereoMode(const VrStereoConfig &config, const std::function<void()> &fun) {
                 Window::beginVrStereoMode(config);
                 fun();
                 Window::endVrStereoMode();
--- lib/test.cpp
+++ lib/test.cpp
@@ -5,22 +5,44 @@
 */
 
 #include "test.hpp"
+#include <vector>
+#include <iostream>
 
-class Bob {
+class Wheel {
     public:
-        std::string face = "\\0v0/";
-        int size = 20;
-        int x = 0;
-        int y = 0;
-        Color color = RED;
-        int speed = 1;
+        size_t size;
+        std::vector<std::string> elements;
+
+        Wheel(size_t size, std::vector<std::string> elems) : size(size)
+        {
+            for (size_t i = 0 ; i < size ; ++i) {
+                if (elems.size() < (i + 1)) {
+                    this->elements.emplace_back("default");
+                } else {
+                    this->elements.emplace_back(elems[i]);
+                }
+            }
+        };
+
+        void addElement(const std::string &elem) {
+            this->size += 1;
+
+            this->elements.push_back(elem);
+        }
 };
+
+Color getPartColor(size_t iterator)
+{
+    std::vector<Color> colors = {RED, BLUE, YELLOW, ORANGE, PINK, GREEN, PURPLE};
+
+    return colors[iterator % colors.size()];
+}
 
 int main()
 {
     LR::Window window;
 
-    window.setTitle("Wheel of chance !")
+    window.setTitle("Wheel of chance (or fortune) !")
             .setSize(window.getMonitorWidth(), window.getMonitorHeight())
             .setTargetFPS(60)
             .setPosition(0, 0)
@@ -29,20 +51,41 @@ int main()
     LR::Cursor::disable();
     LR::Cursor::hide();
 
-    Bob bob;
-    bob.x = window.getWidth() / 2;
-    bob.y = window.getHeight() / 2;
+    Wheel wheel(5, {"1", "2", "3"});
+
+    float sizePart = 0;
+    long double rotate = 0;
+    bool isRotating = false;
+
+    int wheelSize = (float)window.getWidth() / 100;
 
     while (!window.shouldClose())
     {
-        LR::Window::draw([&window]() {
+        if (LR::Keyboard::isKeyDown(KEY_Z)) { wheel.addElement("default"); }
+        if (LR::Keyboard::isKeyPressed(KEY_SPACE)) { isRotating = true; }
+
+        sizePart = 360.0f / (float)wheel.size;
+
+        if (isRotating) {
+            rotate += 5;
+        } else {
+            rotate = 0;
+        }
+
+        wheelSize += (LR::Mouse::getWheelMove() * 50);
+
+        LR::Window::draw([&window, &wheel, &sizePart, &rotate, isRotating, wheelSize]() {
             LR::Window::clearBackground(BLACK);
 
-            LR::Shapes::drawCircleSector((Vector2){(float)window.getWidth() / 2, (float)window.getHeight() / 2},
-                                         (float)window.getWidth() / 4,
-                                         0.0f,
-                                         90.0f,
-                                         0.1f, RED);
+            for (size_t i = 0 ; i < wheel.size ; ++i) {
+                LR::Shapes::drawCircleSector(
+                        LR::Mouse::getPosition(),
+                        wheelSize,
+                        sizePart * i + rotate,
+                        sizePart * (i + 1) + rotate,
+                        0.1f,
+                        getPartColor(i));
+            }
         });
     }
 
